bd2807392a58807626e699011acf69021b8f8afc
kijada <188354e@gmail.com>
2022-04-21 23:47:44 +0200

    File name changer update

--- file_name_changer.py
+++ file_name_changer.py
@@ -1,4 +1,3 @@
-
 import os
 path0 = path = "D:/Documents/PythonProject/PrtScPy/"
 
@@ -16,6 +15,12 @@ def folder_in_dir(path):
         if os.path.isdir(path+list[i]): f = True
     return f
 
+def flip_text(text):
+    old_text = str.split(text[:len(text)], "-")
+    new_text = ""
+    for j in reversed(range(len(old_text))): new_text = new_text + old_text[j] + "-"
+    return(new_text[:-1])
+
 while True:
     path = path0
     print(path)
@@ -28,8 +33,14 @@ while True:
         object_list = os.listdir(path)
         for i in range(len(object_list)):
             file_name = str.split(object_list[i][:len(object_list[i])-4], "_")
+
             if len(file_name[2]) == 10:
                 new_name = file_name[0] + "_" + file_name[2] + "_" + file_name[1] + ".png"
+                #print(new_name)
+                os.rename(path+object_list[i], path+new_name)
+            elif len(file_name[1]) == 10:
+                new_name = file_name[0] + "_" + flip_text(file_name[1]) + "_" + file_name[2] + ".png"
+                #print(new_name)
                 os.rename(path+object_list[i], path+new_name)
         print("Done")
 
